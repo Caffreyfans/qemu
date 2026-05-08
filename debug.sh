af43d9065328d0b059840c83c82a0dc6707373c3
Caffreyfans <Caffreyfans@163.com>
2026-05-08 11:26:36 +0800

    roms: update

--- debug.sh
+++ debug.sh
@@ -3,7 +3,6 @@
 cp roms/edk2/Build/MetaXPkg/DEBUG_GCC/X64/MetaXGopDxe.rom esp
 cp roms/edk2/Build/MetaXPkg/DEBUG_GCC/X64/*.efi esp
   
-#  -device metaxgpu,romfile=esp/MetaXGopDxe.rom \
 build/qemu-system-x86_64 \
   -machine q35 \
   -m 2G \
@@ -11,4 +10,7 @@ build/qemu-system-x86_64 \
   -device metaxgpu,romfile=esp/MetaXGopDxe.rom \
   -bios esp/OVMF.fd \
   -drive format=raw,file=fat:rw:./esp \
-  -serial stdio
+  -serial file:debug.log \
+  -debugcon file:ovmf-debugcon.log \
+  -global isa-debugcon.iobase=0x402 \
+  -S -s
